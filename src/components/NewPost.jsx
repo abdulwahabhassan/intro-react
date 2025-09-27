4eab40edcf5fd8c9b0f1ab9264e39b78c676306a
Hassan Abdulwahab <hassan.abdulwahab@kuda.com>
2025-09-27 22:09:14 +0100

    feature: add event listener

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,18 @@
 import classes from './NewPost.module.css';
 
 function NewPost() {
+    function changeBodyHandler(event) {
+        // The target of the event will be textarea
+        // Calling .value allows us to access the value of the target
+        // when the event was triggered
+        console.log(event.target.value);
+    }
+
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} />
+                <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
             </p>
             <p>
                 <label htmlFor="name">Your name</label>
