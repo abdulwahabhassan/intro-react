b78c6e6a608b091d319f04a17b706c7f952d1f48
Hassan Abdulwahab <hassan.abdulwahab@kuda.com>
2025-09-27 22:45:22 +0100

    feature: state hoisting/lifting

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -3,29 +3,16 @@ import { useState } from 'react'
 // functions that state with `use` in React are called React hooks
 // Hook functions are executed in React component functions
 
-function NewPost() {
-    const [enteredBody, setEnteredBody] = useState('');
-    // Returns exactly two elements: current value and function used to update the value
-    // Calling the state updating function re-renders your React component if the new value
-    // differs from the current value
-
-    function changeBodyHandler(event) {
-        // The target of the event will be textarea
-        // Calling .value allows us to access the value of the target
-        // when the event was triggered
-        setEnteredBody(event.target.value);
-    }
-
+function NewPost(props) {
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+                <textarea id="body" required rows={3} onChange={props.onBodyChange} />
             </p>
-            <p>{enteredBody}</p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required />
+                <input type="text" id="name" required onChange={props.onAuthorChange} />
             </p>
         </form>
     );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,25 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
+import { useState } from 'react';
 
 function PostList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function onBodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function onAuthorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={onBodyChangeHandler} onAuthorChange={onAuthorChangeHandler} />
             <ul className={classes.posts}>
-                <Post author="Hassan" body="React.js is awesome!" />
+                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Mike" body="I am loving this course" />
             </ul>
         </>
