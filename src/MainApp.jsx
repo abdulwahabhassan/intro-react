73cc23c28159bc70fb663ec874edf84e570d1a21
Hassan Abdulwahab <hassan.abdulwahab@kuda.com>
2025-09-27 19:43:00 +0100

    feature: adding props to components

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,10 +1,15 @@
 import Post from "./components/Post";
 
+function add(a, b) {
+    return a + b;
+}
+
 function MainApp() {
+    const answer = add(4, 5);
     return (
         <main>
-            <Post />
-            <Post />
+            <Post author="Hassan" body="React.js is awesome!" />
+            <Post author="Mike" body="I am loving this course"/>
             <Post />
         </main>
     )
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,16 @@
 const names = ['Hassan', 'Abdulwahab'];
 
-function Post() {
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1]
+// Attributes added to components are called props
+// They allow components to take arguments like regular functions do
+// props here is an object that allows access to any custom value
+// passed to the Post component wherever it is used 
+// (I find this weird tho coming from a statically typed OOP language as a mobile developer!)
+
+function Post(props) {
+    props.body
     return <div>
-        <p>{chosenName}</p>
-        <p>React.js is awseome</p>
+        <p>{props.author}</p>
+        <p>{props.body}</p>
     </div>
 }
 
