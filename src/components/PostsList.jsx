0ab31e71812b76b12f41b72869b3b040d4c50b23
Hassan Abdulwahab <hassan.abdulwahab@kuda.com>
2025-09-28 15:29:10 +0100

    feature: handle side effects using useEffect, handle loading loading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,25 +1,58 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from './Modal';
 
 function PostList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+
+    // our locally hosted dummy backend node server
+    // GET is the default fetch function hence we don't need to
+    // configure this request any further
+    fetch('http://localhost:8080/posts')
+    // .then(response => response.json())
+    // .then(data => {
+    //     setPosts(data.posts)
+    // }); 
+    // classic old way of handling the response (not recommended 
+    // here as this would cause an infinite loop due to React updating the state
+    // and rendering this component which triggers the fetch request again 
+    // resulting in an infinite loop)
+    //Use React's useEffect
+
+    // accepts two arguments; a function that returns an object, and a list.
+    // The function will be excuted by React whenever values in the list
+    // (the second argument) changes referred to as dependencies
+    // An empty list will only cause the useEffect to be executed once
+    useEffect(() => {
+        setIsFetching(true);
+        // async/await keywords allows running of asynchronous code
+        // since useEffect itself cannot take an async function that returns a promise,
+        // hence we create a scoped/nested async function here
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts);
+            setIsFetching(false);
+        }
+
+        fetchPosts();
+    }, []);
 
     function addPostHandler(postData) {
-
         // Available in browsers out of the box and can be used to fetch and send http requests/data
         fetch(
             'http://localhost:8080/posts', // our locally hosted dummy backend node server
-            { 
-                method: 'POST', 
+            {
+                method: 'POST',
                 body: JSON.stringify(postData),
                 headers: {
-                  'Content-Type': 'application/json'  
+                    'Content-Type': 'application/json'
                 }
             }
-        ); 
+        );
 
         /*
          This is not optimal but works
@@ -45,7 +78,7 @@ function PostList({ isPosting, onStopPosting }) {
                     />
                 </Modal>
             )}
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                     {/* The key prop which should be a guaranteed unique value 
                 is not a prop you must accept or use in your component.
@@ -53,11 +86,17 @@ function PostList({ isPosting, onStopPosting }) {
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>
             )}
-            {posts.length === 0 && <div style={{ textAlign: 'center', color: 'white' }}>
-                <h2>There are no posts yet.</h2>
-                <p>Start adding some!</p>
-            </div>}
-
+            {!isFetching && posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+            {isFetching && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <p>Loading posts...</p>
+                </div>
+            )}
         </>
     );
 }
